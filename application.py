9fe5980edcead5ce399bd1cdc2001839c04b34be
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-04-27 22:51:49 -0500

    Added a landing page, and code to check the SQL Table for usernames and passwords.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,21 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/Landing", methods=["POST"])
+def Landing():
+    username = request.form.get("Username")
+    password = request.form.get("Password")
+    print(f"{username} and {password}")
+    if db.execute("SELECT * FROM logins WHERE username = :username", {"username": username}).rowcount == 0:
+        db.execute("INSERT INTO logins (username,password) VALUES (:username, :password)",{"username": username, "password": password})
+        db.commit()
+        return f"Welcome to the Landing Page, we have created your account {username}"
+    elif db.execute("SELECT * FROM logins WHERE username = :username", {"username": username}).rowcount == 1:
+        login = db.execute("SELECT * FROM logins WHERE username = :username", {"username" : username}).fetchall()
+        print(login)
+        if login[0][1] == username and login[0][2] == password:
+            return f"Welcome back to the landing page {username}"
+        else:
+            return f"{username} There has been an error, your password does not match your account."
