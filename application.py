c16ad2a48564327d612e527d5043020d5d17b66c
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-04-30 17:56:44 -0500

    Revamped sign in and log out features, added hashing and a logout button.

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 import os
 
 import requests
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -24,34 +25,63 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/Landing", methods=["POST"])
-def Landing():
-    username = request.form.get("Username")
-    password = request.form.get("Password")
-    if username != None and password != None:
-        if db.execute("SELECT * FROM logins WHERE username = :username", {"username": username}).rowcount == 0:
-            db.execute("INSERT INTO logins (username,password) VALUES (:username, :password)",{"username": username, "password": password})
-            db.commit()
-            session["Username"] = username
-            return render_template("Landing.html", username=username)
-        elif db.execute("SELECT * FROM logins WHERE username = :username", {"username": username}).rowcount == 1:
-            login = db.execute("SELECT * FROM logins WHERE username = :username", {"username" : username}).fetchall()
-            print(login)
-            if login[0][1] == username and login[0][2] == password:
-                return render_template("Landing.html", username=username)
-                session["Username"] = username
-            else:
-                return f"{username} There has been an error, your password does not match your account."
+    if session.get("Username") != None:
+        return redirect(url_for("Landing"))
     else:
+        return render_template("Choose.html")
+
+@app.route("/SignUp", methods=["POST","GET"])
+def SignUp():
+    if request.method == "POST":
+        username = request.form.get("Username")
+        password = generate_password_hash(request.form.get("Password"))
+        if db.execute("SELECT * FROM logins WHERE username = :username", {"username":username}).rowcount == 0:
+            session["Username"] = username
+            db.execute("INSERT INTO logins (username,password) VALUES (:username, :password)",{"username":username, "password":password})
+            db.commit()
+            return render_template("Landing.html",username=username)
+        elif db.execute("SELECT * FROM logins WHERE username = :username", {"username":username}).rowcount != 0:
+            return render_template("SignIn.html",error="The username has been taken.")
+        else:
+            return render_template("Error.html",error="Error, something went wrong.")
+    else:
+        return render_template("SignIn.html")
+
+@app.route("/LogIn", methods=["POST","GET"])
+def LogIn():
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("Username")
+        password = request.form.get("Password")
+        if db.execute("SELECT * FROM logins where username = :username", {"username":username}).rowcount == 0:
+            return render_template("LogIn.html",error="We were unable to find an account with that username.")
+        else:
+            userinfo = db.execute("SELECT password FROM logins WHERE username = :username", {"username":username}).fetchone()
+            if check_password_hash(userinfo["password"],password):
+                session["Username"] = username
+                return render_template("Landing.html",username=username)
+            elif not check_password_hash(userinfo["password"],password):
+                return render_template("LogIn.html",error="The password does not apply to the username.")
+            else:
+                return render_template("Error.html",error="Error, something went wrong.")
+    else:
+        return render_template("LogIn.html")
+
+@app.route("/Landing", methods=["POST","GET"])
+def Landing():
+    if request.method == "POST" or (request.method == "GET" and session["Username"]!= None):
         username = session["Username"]
         query = request.form.get("BookInfo")
-        results = db.execute("SELECT * from books WHERE isbn LIKE :query OR title LIKE :query OR author LIKE :query",{"query" : f"%{query}%"}).fetchall()
-        if len(results)==0:
-            return f"{username} we were unable to find any result associated with your search, please try again."
+        if query != None:
+            results = db.execute("SELECT * from books WHERE isbn LIKE :query OR title LIKE :query OR author LIKE :query",{"query" : f"%{query}%"}).fetchall()
+            if len(results)==0:
+                return render_template("Landing.html",noreturn=True,username=username)
+            else:
+                return render_template("Results.html", username=username, results=results)
         else:
-            return render_template("Results.html", username=username, results=results)
+            return render_template("Landing.html",username=session["Username"])
+    else:
+        return render_template("Error.html",error="This page is inaccessable without an account.")
 @app.route("/Landing/<string:book_isbn>", methods=["POST","GET"])
 def BookPage(book_isbn):
     username = session["Username"]
@@ -71,6 +101,8 @@ def BookPage(book_isbn):
     if bookapi.status_code != 200:
         raise Exception("Error, API call unsuccessful")
     data = bookapi.json()
-    print(data)
     bookrating = data["books"][0]["average_rating"]
     return render_template("BookPage.html", information=information, username=username, comments = comments, usercommented = usercommented, avgrate = avgrate, gbooks = bookrating)
+@app.route("/LogOut")
+def Logout():
+    return "logged out!"
